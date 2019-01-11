7401c48ba2b4840b46f2c26de0215f1e8590689c
mwafak <mwafaqarbash3@gmail.com>
2019-01-11 20:12:59 +0100

    reset added

---
+++ shoppingCart/shoping/src/components/navbar.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+class Navbar extends Component {
+  render() {
+    return (
+      <nav className="navbar navbar-light bg-light">
+        <a className="navbar-brand" href="#">
+          Navbar{" "}
+          <span className="badge.badge-pill.badge-secondary">
+            {this.props.totalCounters}
+          </span>
+        </a>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
--- shoppingCart/shoping/src/index.js
+++ shoppingCart/shoping/src/index.js
@@ -1,10 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-
+import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import Counters from "./components/counters";
 import "bootstrap/dist/css/bootstrap.css";
 
-ReactDOM.render(<Counters />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
 serviceWorker.unregister();
