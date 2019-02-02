451a7c415b9c1ae94a19ad23a0affbfc0f8fc952
mwafak <mwafaqarbash3@gmail.com>
2019-02-02 23:21:51 +0100

    class app modifeid

--- bootstrap/botstrap/src/components/Home.jsx
+++ bootstrap/botstrap/src/components/Home.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { Jumbotron, Grid, Row, Col, Image, Button } from "react-bootstrap";
 import "./Home.css";
-export class Home extends Component {
+class Home extends Component {
   render() {
     return (
       <Grid>
