a01964c225cd22bab8aeae860814579bf06055d7
mwafak <mwafaqarbash3@gmail.com>
2019-02-07 22:47:39 +0100

    files added

---
+++ planning-list/src/components/projects/ProjectSummary.js
@@ -0,0 +1,13 @@
+import React from "react";
+const ProjectSummary = () => {
+  return (
+    <div className="card z-depth-0 project-summary">
+      <div className="card-content grey-text text-darken-3 ">
+        <span className="card-title">project title</span>
+        <p>welcome</p>
+        <p className="grey-text">07.02.2019</p>
+      </div>
+    </div>
+  );
+};
+export default ProjectSummary;
