f3b9574df526be1b8849d0359869b2142a2f921e
mwafaka <mwafaqarbash3@gmail.com>
2021-01-02 15:06:10 +0100

    last commit

--- random-quote-machine/random-app/src/QuoteMachine.js
+++ random-quote-machine/random-app/src/QuoteMachine.js
@@ -1,42 +1,44 @@
+import { Button,Card } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React,{Component,Fragment} from 'react';
 
 class QuoteMachine extends Component{
- 
-
-    constructor(props){
+     constructor(props){
         super(props);
         this.state= {
-            items:[]
+            quote:'Never Blame your self, But learn from your Mistakes',
+            author:'Mwafak Arbash'
         }
       }
-      random=()=>{
-       
+      ///////////////
+       random=()=>{ 
         fetch('https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json')
         .then(res=>res.json())
         .then(data=> {
           let qouteData= data.quotes;
           let  randomNumber=Math.floor(Math.random()*qouteData.length);
           let randomData=qouteData[randomNumber]
-          console.log(randomData)
-          this.setState({randomData:this.state.items})
+          
+          this.setState({quote:randomData.quote,author:randomData.author
+          })
         })
-        
-        
-      
+      } 
 
-      }
 
 
     render(){
        
         return(
             <Fragment>
-               <h1>Quote Machine</h1>
-               <button onClick={this.random}>Random</button>
-           
-              <h2>{this.state.items}</h2> 
-            
-
+              <Card  >
+  <Card.Header  >Quotes</Card.Header>
+  <Card.Body >
+    <Card.Title >{this.state.author}</Card.Title>
+    <Card.Text >{this.state.quote}</Card.Text>
+    <Button  onClick={this.random} variant="primary">Go somewhere</Button>
+  </Card.Body>
+</Card>
+              
             </Fragment>
         )
     }
