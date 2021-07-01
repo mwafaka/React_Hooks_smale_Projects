d1862b290403768bd554c62773bc02e872b81a28
mwafaka <mwafaqarbash3@gmail.com>
2021-07-01 12:11:06 +0200

    animation added

--- portfolio/portfolio/src/components/intro/Intro.jsx
+++ portfolio/portfolio/src/components/intro/Intro.jsx
@@ -15,12 +15,13 @@ function Intro() {
     return (
         <div className='Intro' id='intro'>
             <div className="left">
-                <div className="imgContainer">
-                    
-                    <img src="assets/man.jpg" alt= ""/>
-                    
-                    </div>
+            <div className="imgContainer"> 
+             <img src="assets/man.jpg" alt= ""/> 
             </div>
+
+            
+         </div>
+        
             <div className="right">
                 <div className="wrapper">
                     <h2>Hi There, I ’m </h2>
@@ -30,6 +31,17 @@ function Intro() {
             <a href='#portfolio'>
            <img src='assets/angle_Down.png' alt=''/>
             </a>
+            </div>
+            <div className="bubbles">
+                <img src='./assets/html.png' alt=''/>
+                <img src='./assets/css.png' alt=''/>
+                <img src='./assets/js.png' alt=''/>
+                <img src='./assets/react.png' alt=''/>
+                <img src='./assets/node.png' alt=''/>
+                <img src='./assets/python.png' alt=''/>
+                <img src='./assets/mongo.png' alt=''/>
+
+             
             </div>
         </div>
     )
